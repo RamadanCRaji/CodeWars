0f49e1bbfc6157b6f4b8a863876e3764ee4b08a8
Ramadan E. Raji <armadanraji@gmail.com>
2023-06-11 17:21:21 -0500

    refractored the code to be more DRY

--- CC_06_12_23/app.js
+++ CC_06_12_23/app.js
@@ -1,10 +1,4 @@
 /*
-----------------------
-challenge 1
-----------------------
-*/
-
-/*
 We want to create a constructor function 'NameMe', which takes first name and last name as parameters. The function combines the first and last names and saves the value in "name" property.
 We already implemented that function, but when we actually run the code, the "name" property is accessible, but the "firstName" and "lastName" is not accessible. All the properties should be accessible. Can you find what's wrong with it? A test fixture is also available
 
@@ -20,16 +14,14 @@ n.lastName //Expected: Doe
 n.name //Expected: John Doe
 
 */
-
-// solution
-// P: will paramerters always be strings? Yes
-// R: Return all the values of the properties but specifically the name property
-// E: if n=new NameMe('John', 'Doe');, n.name should return: John Doe
-//    n.name should return: John Doe
-//    n.firstName //Expected: John
-//    n.lastName //Expected: Doe
-
 /*
+solution
+P: will paramerters always be strings? Yes
+R: Return all the values of the properties but specifically the name property
+E: if n=new NameMe('John', 'Doe');, n.name should return: John Doe
+   n.name should return: John Doe
+   n.firstName //Expected: John
+   n.lastName //Expected: Doe
 P: the name property needs to have 'this.' added to it and removed from the object literal notation. 
     remove the object literal notation
 */
@@ -37,4 +29,10 @@ function NameMe(first, last) {
   this.firstName = first;
   this.lastName = last;
   this.name = this.firstName + " " + this.lastName;
+}
+//refractoring code
+function NameMe(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+  this.name = `${this.firstName} ${this.lastName}`;
 }
