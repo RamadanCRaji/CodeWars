fca72df07527fe2fb52b94ca8eb19f681300c58e
Ramadan E. Raji <armadanraji@gmail.com>
2023-06-26 18:27:51 -0500

    solved the kata with the right solution

--- CC_2023_06_25/app.js
+++ CC_2023_06_25/app.js
@@ -21,30 +21,62 @@ function getSum(a, b) {
   if (a === b) {
     return b;
   } else if (a < 0 && b > 0) {
-    for (let i = a; i++; i <= b) {
+    for (let i = a; i <= b; i++) {
       newArr.push(i);
     }
   } else if (a > 0 && b < 0) {
-    for (let i = b; i++; i <= a) {
+    for (let i = b; i <= a; i++) {
       newArr.push(i);
     }
   } else if (a > 0 && b > 0 && a < b) {
-    for (let i = a; i++; i <= b) {
+    for (let i = a; i <= b; i++) {
       newArr.push(i);
     }
   } else if (a > 0 && b > 0 && b < a) {
-    for (let i = b; i++; i <= a) {
+    for (let i = b; i <= a; i++) {
       newArr.push(i);
     }
   } else if (a < 0 && b < 0 && a < b) {
-    for (let i = a; i++; i <= b) {
+    for (let i = a; i <= b; i++) {
       newArr.push(i);
     }
   } else if (a < 0 && b < 0 && b < a) {
     // b=-5 a=-1
-    for (let i = b; i++; i <= a) {
+    for (let i = b; i <= a; i++) {
       newArr.push(i);
     }
   }
   return newArr.reduce((acc, nextItem) => acc + nextItem, 0);
 }
+// correction
+
+function GetSum(a, b) {
+  let newArr = [];
+  if (a < b) {
+    for (let i = a; i <= b; i++) {
+      newArr.push(i);
+    }
+  } else if (a > b) {
+    for (let i = a; i >= b; i++) {
+      newArr.push(i);
+    }
+  }
+}
+/*
+function GetSum(a, b) {
+  sum = 0;
+  if (a < b) {
+    while (a <= b) {
+      sum = sum + a;
+      a++;
+    }
+  } else {
+    while (a >= b) {
+      sum = sum + a;
+      a--;
+    }
+  }
+  return tmp;
+}
+
+*/
