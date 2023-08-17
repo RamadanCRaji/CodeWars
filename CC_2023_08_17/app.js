06bef0a89c3279bf13454e0789543d3238a79537
Ramadan E. Raji <armadanraji@gmail.com>
2023-08-17 08:53:38 -0500

    Returning strings

---
+++ CC_2023_08_17/app.js
@@ -0,0 +1,15 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+/*
+P: name input
+R:
+    return a greating that includes the parameter
+E:
+ "Hello, <name> how are you doing today?"
+P
+    use template literals to concate the return str
+
+*/
+function greet(name) {
+    return `Hello, ${name} how are you doing today?`;
+}
