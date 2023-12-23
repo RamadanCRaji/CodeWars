15eff03522c4de9e57d6e6b634811e0c19720eb3
Ramadan E. Raji <armadanraji@gmail.com>
2023-12-23 15:07:55 -0600

    rewrote with the correct logic

--- CC_2023_12_23/app.js
+++ CC_2023_12_23/app.js
@@ -33,14 +33,14 @@ function alphabetWar(fights) {
     let leftSidePowers, rightSidePowers, righSideChars, leftSidechars, rightParticpants, leftParticpants;
     leftSidePowers = { w: 4, p: 3, b: 2, s: 1 };
     rightSidePowers = { m: 4, q: 3, d: 2, z: 1 };
-    righSideChars = 'wpbs';
-    leftSidechars = 'mqdz';
+    leftSidechars  = 'wpbs';
+    righSideChars = 'mqdz';
     rightParticpants = fights.split('')
         .filter(e => righSideChars.includes(e))
-        .reduce((accum, nextItem) => accum + rightSidePowers['nextItem'], 0)
+        .reduce((accum, nextItem) => accum + rightSidePowers[nextItem], 0)
     leftParticpants = fights.split('')
         .filter(e => leftSidechars.includes(e))
-        .reduce((accum, nextItem) => accum + rightSidePowers['nextItem'], 0)
+        .reduce((accum, nextItem) => accum + leftSidePowers[nextItem], 0)
     if (rightParticpants > leftParticpants) {
         return 'Right side wins!'
     }
