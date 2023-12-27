c62106b8865809fbe944fea2b5829418e16b1448
Ramadan E. Raji <armadanraji@gmail.com>
2023-12-27 15:08:01 -0600

    switching letters in str

---
+++ CC_2023_12_27/app.js
@@ -0,0 +1,40 @@
+// Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+// Example:
+
+// 'acb' --> 'bca'
+// 'aabacbaa' --> 'bbabcabb'
+
+/**
+ * parameter
+    * take in a str input
+ * return 
+    * new string with all letter c swapped with letter b and vice versa
+ * 
+ * 
+ * 
+ */
+//example
+
+function switcheroo(str) {
+    /**
+     * split string into an array
+     * use arr.map to transform elligible character
+     * create a helper function to make the switch 
+     * if a or b use the helper function else return element
+     * convert the array into string with arr.join
+     * then return 
+     */
+    let strArr = str.split('').map(e => {
+        if (e === 'a' || e === 'b') {
+            return makeSwitch(e)
+        }
+        else { return e }
+    })
+    return strArr.join('')
+}
+function makeSwitch(e) {
+    return e === 'a' ? 'b' : 'a'
+}
+console.log(switcheroo('abdfcs')) // -> acdfbs
+console.log(switcheroo('aaabcccbaaa')) // bbbacccabbb
