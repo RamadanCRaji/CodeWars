b8b1fb98fc0f87f4f8170b60da829c6923013d96
Ramadan E. Raji <armadanraji@gmail.com>
2024-03-09 13:28:49 -0600

    counting positives and summing negatives

---
+++ codeWars2023/CC_2024_03_09/app.js
@@ -0,0 +1,33 @@
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+/*
+P
+  input will be an array  
+R
+  return a new array where the first is toatl of # > 0 and second element is sum of sum of # <0
+
+PsedoCode
+  filter out positive numbers & use arr.length to total +ve #s
+  filter the numbers that are <0 and use reduce method and reutrn the sum of -ve #s
+  return arr.concate whch shows the desired 
+
+*/
+function countPositivesSumNegatives(input) {
+   if (!Array.isArray(input) || !input.length) return [];
+
+   let totalPositveNum = input.filter((e) => e > 0).length;
+   let sumofNegative = input
+      .filter((e) => e < 0)
+      .reduce((acc, next) => acc + next, 0);
+   let finalAns = [];
+   finalAns.push(totalPositveNum);
+   finalAns.push(sumofNegative);
+   return finalAns;
+}
+
+console.log(
+   countPositivesSumNegatives([
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15,
+   ]),
+   [10, -65]
+);
