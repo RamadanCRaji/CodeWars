c9b2a889878f8dfbff0f450861fc64367e2ee422
Ramadan E. Raji <armadanraji@gmail.com>
2024-05-01 13:57:02 -0500

    counting reoccuring characters in strings

--- codeWars2023/CC_2024_05_01/app.js
+++ codeWars2023/CC_2024_05_01/app.js
@@ -6,11 +6,34 @@
 
 /*
 P
+   input is a string only  
 R
+    return {} containing # characters in key values
 E
 P
+    change the string to an array
+    create a varible of type 'object' {}
+    loop through the input 
+        check if empty object has the key-value pair?
+            key-value pair? incremeant: create and then increament
+            do this using a helper function
+        return answer
  */
 function count(string) {
    // TODO
-   return {};
+   const charaCount = {};
+   const doesExist = (item) => charaCount[item];
+   string.split("").forEach((e) => {
+      if (doesExist(e)) {
+         charaCount[e] = charaCount[e] + 1;
+      } else if (!doesExist(e)) {
+         charaCount[e] = 1;
+      }
+   });
+   return charaCount;
 }
+
+//Example
+console.log(count("aba")); // => {a:2,b:1}
+console.log(count("hahakkkkaaa")); //{h:2,a:5,k:4}
+console.log(count(""));
