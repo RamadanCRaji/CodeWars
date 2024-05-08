bb3a20b3a38905439cfc2c998cd2ea7158848a30
Ramadan E. Raji <armadanraji@gmail.com>
2024-05-08 10:11:46 -0500

    title case correct solution

--- codeWars2023/CC_2024_05_05/app.js
+++ codeWars2023/CC_2024_05_05/app.js
@@ -13,44 +13,71 @@
 // titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
 // titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
 
+/*
+    return a string with title Case and any minor words that's gets lowercased except if the word is the first word of the string then it is titleCased
+ 
+P: 
+    two arguments both strings- p1:title, p2:minorWords
+R:
+    return p1 where all words in str are 'Title Cased'
+
+P:
+    condition checker
+        two outcomes 
+        if (minorWords)
+            split strings to appray and check if some of the elements are present in titleWord
+            remove first character in array and then check if minorWords includes it
+                if it does then camelCAse using splice
+            now camelCase the rest and then check if minorWors includes it, 
+                if it does then loop through arr and lowerCase items that are true
+                return 
+            create a helper function
+        {
+
+        }
+        else{
+            convert all strings in title to Title Case
+        }
+
+*/
 //Example
 titleCase("a clash of KINGS", "a an the of"); //=> 'A Clash of Kings'
 // titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows
 
-/*
-    return a string with title Case and any minor words that's gets lowercased except if the word is the first word of the string then it is titleCased
- 
-P:
-    condition checker
-        check if the first word of the string is present in the minor string
-            - if yes
-                - this will lead to camel that first word 
-                - any other word matching that same letter will be lowercased
-        check if any other word aside from first letter appears in minor
-            - if yes,
-                - those words get lower cased regardless
-        check if that word is not the first word in the string or present at all
-            - then it will get camel cased
-    - split the string into an array
-    - convert all elements into camelCase
-    - use Array.shift to remove the first word of the string
-    - map through the array the remaining elements and inside check if the minor words has contains current item
-        - if yes convert to lowerCase and return
-    - use arr.concat to combine all element
+// function titleCase(title, minorWords) {
+//    const makeCamelCase = (e) =>
+//       e
+//          .toString()
+//          .split(" ")
+//          .map((e) => e[0].toUpperCase() + e.slice(1).toLowerCase());
+//    const isMinorWords = (e) =>
+//       title.split(" ").some((e) => minorWords.includes(e));
+//    const makeLoweCase = (e) => e.toLowerCase();
 
+//    if (minorWords) {
+//       if (isMinorWords(title)) {
+//          let titleCased = [...title.split(" ")].map((e) =>
+//             minorWords.includes(e) ? makeCamelCase(e) : makeCamelCase(e)
+//          );
+//          let firstWord = titleCased.splice(0, 1).map((e) => makeCamelCase(e));
+//          return firstWord.concat(titleCased);
+//       }
+//    } else {
+//       return makeCamelCase(title);
+//    }
 
-*/
+// }
 
-function titleCase(title, minorWords) {
-   let titleCasedWords = title.split(" ").map((e) => {
-      let value = e.slice(0, 1).toUpperCase() + e.slice(1).toLowerCase();
-      return value;
-   });
-   let firstWord = [...titleCasedWords.shift()].join("");
-
-   let restOfWords = titleCasedWords.map((e) =>
-      minorWords.includes(e.toLowerCase()) ? e.toLowerCase() : e
-   );
-   const finalAns = firstWord.concat(restOfWords);
-   return finalAns;
+function titleCase(title, minorWords = "") {
+   const minorWordsArray = minorWords.toLowerCase().split(" ");
+   const titleCased = title
+      .toLowerCase()
+      .split(" ")
+      .map((word, index) => {
+         if (index !== 0 && minorWordsArray.includes(word)) {
+            return word;
+         }
+         return word.charAt(0).toUpperCase() + word.slice(1);
+      });
+   return titleCased.join(" ");
 }
