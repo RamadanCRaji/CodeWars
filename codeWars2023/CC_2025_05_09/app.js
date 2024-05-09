c1632f905ad0d5e1323feaa9ac7548c6ca38ce6f
Ramadan E. Raji <armadanraji@gmail.com>
2024-05-09 14:01:00 -0500

    proper function declaration

--- codeWars2023/CC_2025_05_09/app.js
+++ codeWars2023/CC_2025_05_09/app.js
@@ -1,5 +1,23 @@
 // A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
 
-function main [verb, noun]
-  return verb + noun
+// function main [verb, noun]
+//   return verb + noun
+// }
+
+/**
+ a proper function consist of
+    - A function name 
+    - A function body which is surrounded by a curly braces
+    - Parameter inside of a parenthesis 
+
+R
+    return the function with the correct form
+
+P:
+    no closing curly braces
+    used an array literal to enclose the parametes instead of ()
+ */
+
+function main(verb, noun) {
+   return verb + noun;
 }
