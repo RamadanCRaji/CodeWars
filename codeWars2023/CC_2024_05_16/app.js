a3e1dddc2898fa173b7e696bdc0071fc65fbd975
Ramadan E. Raji <armadanraji@gmail.com>
2024-05-16 11:19:42 -0500

    mixed array of numbers

---
+++ codeWars2023/CC_2024_05_16/app.js
@@ -0,0 +1,30 @@
+// Given a mixed array of number and string representations of integers, add up the non-string integers and subtract the total of the string integers.
+
+// Return as a number.
+
+/*
+P:
+   input is arr [strings of int, int]
+R
+   return the sum of (int)-sum of (string Int)
+P
+   loop through the array and filter out strings and also numbers
+   use the reduce method obtain the sum of each data type 
+   then using reduce to obtain one more final value and return ans 
+*/
+function divCon(x) {
+   let sumOfString = Array.from(x, (C) => typeof C === "string").reduce(
+      (accum, nextItem) => +accum + +nextItem,
+      0
+   );
+   console.log(sumOfString);
+   let sumOfInt = Array.from(x, (C) => typeof C === "Number").reduce(
+      (accum, nextItem) => accum + nextItem,
+      0
+   );
+   return sumOfInt - sumOfString;
+}
+
+//Example
+const value1 = ["5", "0", 9, 3, 2, 1, "9", 6, 7];
+console.log(divCon(value1)); // ==> ans =14
