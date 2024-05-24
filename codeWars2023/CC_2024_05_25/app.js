edbdf8b1786aae8a82bd8863683faf0bf78c6d1f
Ramadan E. Raji <armadanraji@gmail.com>
2024-05-24 17:23:06 -0500

    converting vowels to 1 and non vowels to 0

---
+++ codeWars2023/CC_2024_05_25/app.js
@@ -0,0 +1,21 @@
+// vowelOne
+// Write a function that takes a string and outputs a strings of 1's and 0's where vowels become 1's and non-vowels become 0's.
+
+// All non-vowels including non alpha characters (spaces,commas etc.) should be included.
+
+// Examples:
+
+// vowelOne( "abceios" ) // "1001110"
+
+// vowelOne( "aeiou, abc" ) // "1111100100"
+
+function vowelOne(s) {
+   let ans = s.split("").map((item) => {
+      if ("aeiou".includes(item.toLowerCase())) {
+         return "1";
+      } else {
+         return "0";
+      }
+   });
+   return ans.join("");
+}
